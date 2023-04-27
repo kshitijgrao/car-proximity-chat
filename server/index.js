5d532c706e17c6746e5346e141de9a982014d55b
Nindroz <mantha.hitesh@gmail.com>
2023-04-27 09:13:58 -0700

    distance calculations done (untested)

--- server/index.js
+++ server/index.js
@@ -18,7 +18,24 @@ wss.on("connection", function (ws, req) {
     if (out[0] != 0) {
       arr.set(out[0], new Array(out[1], out[2]));
     }
-    console.log(out[0]);
-    console.log(arr);
+    console.log(outputDistanceArray());
   });
 });
+
+function outputDistanceArray(localUID) {
+  const localLoc = new Array(arr.get(localUID));
+
+  const output = new Map();
+  for (uid in arr) {
+    remLoc = arr.get(uid);
+    output.set(
+      uid,
+      outputdistanceFormula(localLoc[0], localLoc[1], remLoc[0], remLoc[1])
+    );
+  }
+  return output;
+}
+
+function distanceFormula(x1, y1, x2, y2) {
+  return Math.sqrt(x2 * x2 - x1 * x1 + (y2 * y2 - y1 * y1));
+}
