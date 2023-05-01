d2ac3b285cb4684173141c19854f8913c8ed83a4
s-ksrao <67560587+s-ksrao@users.noreply.github.com>
2023-05-01 09:14:39 -0700

    started working on page control

--- Mini-Projects/base_app/lib/main.dart
+++ Mini-Projects/base_app/lib/main.dart
@@ -4,6 +4,53 @@ void main() {
   runApp(const MyApp());
 }
 
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  final String s = 'Press this button to join Proximity Chat!';
+  final Color c = const Color.fromARGB(255, 30, 113, 196);
+  final Icon i = const Icon(Icons.car_rental_rounded);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Demo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              s,
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            // Text(
+            //   '$_counter',
+            //   style: Theme.of(context).textTheme.headlineMedium,
+            // ),
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const CallJoinPage(title: 'SecondPage');
+                }));
+              },
+              tooltip: 'Join Chat!',
+              backgroundColor: c,
+              child: i,
+            )
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -24,31 +71,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Car Proximity Chat'),
+      home: const FirstPage(title: 'Car Proximity Chat'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
 class CallJoinPage extends StatelessWidget {
-  const CallJoinPage({Key key, required this.title}) : super(key: key);
+  const CallJoinPage({Key? key, required this.title}) : super(key: key);
   final String title;
   @override
   Widget build(BuildContext context) {
@@ -62,114 +91,6 @@ class CallJoinPage extends StatelessWidget {
           child: const Text('Go Back'),
         ),
       ),
-    );
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-  int _counter2 = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
-
-  void _incrementCounter2() {
-    setState(() {
-      _counter2++;
-      //print(_counter2);
-    });
-  }
-
-  String s = 'Press this button to join Proximity Chat!';
-  Color c = const Color.fromARGB(255, 30, 113, 196);
-  Icon i = const Icon(Icons.car_rental_rounded);
-
-  void _setButtonState() {
-    setState(() {
-      _incrementCounter2();
-      if (_counter2 % 2 == 0) {
-        s = ('Press this button to join Proximity Chat!');
-        c = const Color.fromARGB(255, 30, 113, 196);
-        i = const Icon(Icons.car_repair);
-      } else {
-        s = ('You are successfully connected to Proximity Chat.');
-        c = const Color.fromARGB(255, 245, 101, 34);
-        i = const Icon(Icons.phone);
-      }
-      //print(s);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              // if (_counter2 % 2 == 0) {
-              //   'Press the Button to Join Proximity Chat!'
-              // } else {
-              //   'You are connected to Proximity Chat.'
-              // }
-              s,
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            // Text(
-            //   '$_counter',
-            //   style: Theme.of(context).textTheme.headlineMedium,
-            // ),
-            FloatingActionButton(
-              onPressed: _setButtonState,
-              tooltip: 'Join Chat!',
-              backgroundColor: c,
-              child: i,
-            )
-          ],
-        ),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
